8b92194264809e3a1dfd3c988affd9859d7aee14
Manngez <121944912+Manngez@users.noreply.github.com>
2026-08-10 14:58:15 +0200

    Add Geoline script loader

---
+++ app.js
@@ -0,0 +1,6 @@
+'use strict';
+// Geoline is split into three classic scripts for maintainability. This loader runs
+// during document parsing so all modules are available before DOMContentLoaded.
+document.write('<script src="app-core.js"><\/script>');
+document.write('<script src="app-game.js"><\/script>');
+document.write('<script src="app-online.js"><\/script>');
