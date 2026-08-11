1a66c1391870335b7cd9e2f2ce9f2895094be4b7
Manngez <121944912+Manngez@users.noreply.github.com>
2026-08-12 01:38:21 +0200

    Load USA and Canada country mode

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 // Geoline is split into classic scripts for maintainability. This loader runs
 // during document parsing so all modules are available before DOMContentLoaded.
 document.write('<link rel="stylesheet" href="game-ui.css">');
+document.write('<link rel="stylesheet" href="country-mode.css">');
 document.write('<script src="app-core.js"><\/script>');
 document.write('<script src="app-map.js"><\/script>');
 document.write('<script src="app-game.js"><\/script>');
 document.write('<script src="settlement-filter.js"><\/script>');
 document.write('<script src="app-online.js"><\/script>');
+document.write('<script src="country-mode.js"><\/script>');
 document.write('<script src="game-ui.js"><\/script>');
