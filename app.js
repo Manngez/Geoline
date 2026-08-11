dfc36d94e9b250429e625f8dfbe176b074b9a790
Manngez <121944912+Manngez@users.noreply.github.com>
2026-08-12 01:44:18 +0200

    Load expanded place search

--- app.js
+++ app.js
@@ -9,4 +9,5 @@ document.write('<script src="app-game.js"><\/script>');
 document.write('<script src="settlement-filter.js"><\/script>');
 document.write('<script src="app-online.js"><\/script>');
 document.write('<script src="country-mode.js"><\/script>');
+document.write('<script src="place-search-v2.js"><\/script>');
 document.write('<script src="game-ui.js"><\/script>');
